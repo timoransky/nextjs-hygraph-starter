b6d5c59db86372ee11b4ef8a5ea130a5c32bbdbd
Ján Timoranský <15653065+timoransky@users.noreply.github.com>
2025-04-04 22:06:37 +0200

    feat: add hygraph live preview

---
+++ src/app/api/draft/route.ts
@@ -0,0 +1,33 @@
+import { cookies, draftMode } from "next/headers";
+import { redirect } from "next/navigation";
+
+export async function GET(request: Request) {
+  // Parse query string parameters
+  const { searchParams } = new URL(request.url);
+  const secret = searchParams.get("secret");
+  const model = searchParams.get("model");
+  const slug = searchParams.get("slug");
+
+  if (secret !== process.env.HYGRAPH_PREVIEW_SECRET && !model && !slug) {
+    return new Response("Missing parameters", { status: 400 });
+  }
+
+  // Enable Draft Mode by setting the cookie
+  const draft = await draftMode();
+  draft.enable();
+
+  // Update the cookie and set same site to none so we can render it inside Hygraph
+  const cookieStore = await cookies();
+  const cookie = cookieStore.get("__prerender_bypass");
+  cookieStore.set({
+    name: "__prerender_bypass",
+    value: cookie?.value ?? "",
+    httpOnly: true,
+    path: "/",
+    secure: true,
+    sameSite: "none",
+  });
+
+  // Redirect to the path
+  redirect(`/${model}/${slug}`);
+}
--- src/lib/hygraph/index.ts
+++ src/lib/hygraph/index.ts
@@ -1,25 +1,25 @@
 import { GraphQLClient } from "graphql-request";
 import { getSdk } from "./__generated/graphql";
 
-export const endpoint = process.env.HYGRAPH_ENDPOINT || "";
+const endpoint = process.env.HYGRAPH_ENDPOINT || "";
 
-export const HygraphClient = (
-  { preview }: { preview: boolean } = { preview: false }
-) => {
+const createHygraphClient = (isDraftMode = false) => {
   if (!endpoint) {
     throw new Error("HYGRAPH_ENDPOINT is not defined");
   }
 
   const client = new GraphQLClient(endpoint);
 
-  if (preview) {
+  if (isDraftMode) {
     client.setHeader(
       "Authorization",
       `Bearer ${process.env.HYGRAPH_PREVIEW_TOKEN}`
     );
   }
 
-  return client;
+  return getSdk(client);
 };
 
-export const hygraph = getSdk(HygraphClient({ preview: false }));
+export const getHygraphSDK = (isDraftMode = false) => {
+  return createHygraphClient(isDraftMode);
+};
