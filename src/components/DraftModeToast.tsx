f3b8d1f1e945212599cbe7f2c81cd897bf4966a2
Ján Timoranský <15653065+timoransky@users.noreply.github.com>
2025-04-06 22:06:20 +0200

    feat: add shadcn

---
+++ src/components/DraftModeToast.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { disableDraftMode } from "@/app/actions";
+import { useRouter } from "next/navigation";
+import { useEffect, useTransition } from "react";
+import { toast } from "sonner";
+
+export const DraftModeToast = () => {
+  const router = useRouter();
+  const [pending, startTransition] = useTransition();
+
+  useEffect(() => {
+    // Show the toast only if we are not in the Hygraph live preview iframe
+    const isIframe =
+      typeof window !== "undefined" && window.self !== window.top;
+
+    if (!isIframe) {
+      const toastId = toast("Draft Mode Enabled", {
+        description: "You are currently previewing drafts",
+        duration: Infinity,
+        action: {
+          label: "Disable",
+          onClick: async () => {
+            await disableDraftMode();
+            startTransition(() => {
+              router.refresh();
+            });
+          },
+        },
+      });
+      return () => {
+        toast.dismiss(toastId);
+      };
+    }
+  }, [router]);
+
+  useEffect(() => {
+    if (pending) {
+      const toastId = toast.loading("Disabling draft mode...");
+      return () => {
+        toast.dismiss(toastId);
+      };
+    }
+  }, [pending]);
+
+  return null;
+};
---
+++ src/components/ui/sonner.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Toaster as Sonner, ToasterProps } from "sonner";
+
+const Toaster = ({ ...props }: ToasterProps) => {
+  // Implement dark mode support using this hook
+  // const { theme = "system" } = useTheme();
+
+  return (
+    <Sonner
+      theme="light"
+      className="toaster group"
+      style={
+        {
+          "--normal-bg": "var(--popover)",
+          "--normal-text": "var(--popover-foreground)",
+          "--normal-border": "var(--border)",
+        } as React.CSSProperties
+      }
+      {...props}
+    />
+  );
+};
+
+export { Toaster };
