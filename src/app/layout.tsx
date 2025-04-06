177a88c95956fbd5afe71144fd6bd79baf7bc008
Ján Timoranský <15653065+timoransky@users.noreply.github.com>
2025-04-06 22:12:09 +0200

    fix: renamed component to kebab-case

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { draftMode } from "next/headers";
-import { DraftModeToast } from "@/components/DraftModeToast";
+import { DraftModeToast } from "@/components/draft-mode-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
